2d7ee0c0248975d462d1d135d779bc7828828140
Raoul de Charette <5967026+rdecharette@users.noreply.github.com>
2024-12-09 18:26:34 +0100

    update

--- fr_stats.py
+++ fr_stats.py
@@ -54,7 +54,7 @@ def is_french(list):
 
 conf_path = os.path.join('thirdparty','paperlists',conf)
 f = open(conf_path)
-data = json.load(f)
+data = np.asarray(json.load(f))
 
 
 
@@ -64,26 +64,13 @@ for i, paper in enumerate(tqdm(data)):
     aff = paper["aff"]
     # aff = re.sub("(?i)university", "uni.", paper["aff"])
     # aff = re.sub("(?i)institute", "inst.", paper["aff"])
+    paper["authors"] = string_preprocess(paper["author"].split(", "))
     paper["affiliations"] = string_preprocess(np.asarray(aff.split("; ")))
     paper["affiliations"] = paper["affiliations"][paper["affiliations"]!=""]
 
 aff_all = np.hstack([paper["affiliations"] for paper in data])
 aff_u, aff_u_c = np.unique(aff_all, return_counts=True)
 aff_stats = d = {**dict(zip(aff_u, aff_u_c))}
-
-
-
-print("Count authors affiliations")
-plt.figure()
-s = aff_u_c.argsort()[::-1][:25]
-sns.barplot(aff_u_c[s], edgecolor="black")
-plt.xticks(range(len(aff_u_c[s])), [s if len(s) < 35+5 else s[:35]+" (..)" for s in aff_u[s]], rotation=45, ha='right')
-
-# plt.xlabel("Affiliations")
-plt.ylabel("Count")
-plt.title("Authors affiliations\n(can be more than one per author/paper)")
-plt.tight_layout()
-plt.savefig("author_affiliations.png")
 
 
 
@@ -101,34 +88,22 @@ sns.barplot(aff_u_papers[s], edgecolor="black")
 plt.xticks(range(len(aff_u_papers[s])), [s if len(s) < 35+5 else s[:35]+" (..)" for s in aff_u[s]], rotation=45, ha='right')
 
 plt.ylabel("Count")
-plt.title("Paper affiliations")
+plt.title("%d Affiliations\n(a paper can have multiple affiliations)"%(sum(aff_u_papers)))
 plt.tight_layout()
 plt.savefig("papers_affiliations.png")
 
 
 
-print("Count French authors affiliations")
+print("Compute FR affiliations")
 fr_mask = is_french(aff_u)
-
 aff_fr_u = aff_u[fr_mask]
 aff_fr_u_c = aff_u_c[fr_mask]
 
 print("Found FR affiliations: ", aff_fr_u)
 
-plt.figure()
-s = aff_fr_u_c.argsort()[::-1][:25]
-sns.barplot(aff_fr_u_c[s], edgecolor="black")
-plt.xticks(range(len(aff_fr_u_c[s])), [s if len(s) < 35+5 else s[:35]+" (..)" for s in aff_fr_u[s]], rotation=45, ha='right')
-
-# plt.xlabel("Affiliations")
-plt.ylabel("Count")
-plt.title("French authors affiliations\n(can be more than one per author/paper)")
-plt.tight_layout()
-plt.savefig("fr_authors_affiliations.png")
 
 
-
-print("Count paper w/ French affiliations")
+print("French affiliations")
 aff_fr_u_papers = np.zeros(aff_fr_u.shape)
 for i, aff in enumerate(tqdm(aff_fr_u)):
     aff_fr_u_papers[i] = np.sum([np.any(paper["affiliations"]==aff) for paper in data])
@@ -139,7 +114,7 @@ sns.barplot(aff_fr_u_papers[s], edgecolor="black")
 plt.xticks(range(len(aff_fr_u_papers[s])), [s if len(s) < 35+5 else s[:35]+" (..)" for s in aff_fr_u[s]], rotation=45, ha='right')
 
 plt.ylabel("Count")
-plt.title("Paper w/ French affiliations")
+plt.title("%d French affiliations\n(a paper can have multiple FR affiliations)"%(sum(aff_fr_u_papers)))
 plt.tight_layout()
 plt.savefig("fr_papers_affiliations.png")
 
@@ -148,7 +123,11 @@ data_fr = np.zeros(len(data), dtype=int)
 for i, paper in enumerate(tqdm(data)):
     data_fr[i] = np.sum([np.any(paper["affiliations"]==a_fr) for a_fr in aff_fr_u])
 
+data_fr_authors = np.unique(np.hstack([paper["authors"] for paper in data[data_fr>0]]))
+
 print("Total affiliations: ", sum(data_fr))
 print("Paper w/ at least 1 FR affiliation: ", sum(data_fr>0))
+print("     # of authors: ", len(data_fr_authors))
+print("     authors: ", data_fr_authors)
 
 plt.show()
